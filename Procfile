824f04dad2088a9017185527a045bc48c0eb8e39
Tomibbbb <victoriabanjo16@gmail.com>
2022-07-07 11:21:35 +0100

    uhhm

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn-b "0.0.0.0:$PORT" -w3 blog.wsgi
+release: python3 manage.py migrate
--- mysite/settings.py
+++ mysite/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticriles')
+STATICFILES_STORAGE = 'whitenoise.storage.compressed ManifestStaticFileStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
