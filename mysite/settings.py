961901d47e4d815b5baaa3f4bc53f98b9d231c28
Tomibbbb <victoriabanjo16@gmail.com>
2022-07-07 13:02:05 +0100

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import dj_database_url
+
 from pathlib import Path
 from dotenv import load_dotenv,find_dotenv
+import dj_database_url
 
 load_dotenv(find_dotenv())
 
@@ -29,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["blog-vc.herokuapp.com"]
 
