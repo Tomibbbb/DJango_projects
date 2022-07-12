ffca185d88aec3c5b8745ffa4548837fd71a6990
Tomibbbb <victoriabanjo16@gmail.com>
2022-07-12 17:12:40 +0100

    adding comment section

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib import admin
 from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -13,5 +14,10 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+       list_display = ('name', 'email', 'post', 'created', 'active')
+       list_filter = ('active', 'created', 'updated')
+       search_fields = ('name', 'email', 'body')
 
 # Register your models here.
